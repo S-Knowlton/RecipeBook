c06bc4aa28a73a5a2afc0223b32fefa2e6dcf5a9
S-Knowlton <Samantha.l.knowlton@gmail.com>
2022-03-07 12:31:26 -0800

    More set up and model start

---
+++ RecipeBook/RecipeBook/Models/Ingredient.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeBook.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Amount { get; set; }
+
+    }
+}
---
+++ RecipeBook/RecipeBook/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿namespace RecipeBook.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Ingredient[] Ingredients { get; set; }
+
+        public Recipe(string name, Ingredient[] ingredients)
+        {
+            Name = name;
+            Ingredients = ingredients;
+        }
+    }
+}
